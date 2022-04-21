765e7d8fbec9e5f0c7114b8740e89bb994e22632
5h140w <5h140w23@gmail.com>
2022-04-21 17:45:54 +0100

    day1

--- day1/script.js
+++ day1/script.js
@@ -1,1 +1,11 @@
-console.log("ok")
+const hour = document.querySelector("#hour")
+const minute = document.querySelector("#minute")
+const second = document.querySelector("#second")
+
+setInterval(()=>{
+    let currentDay = new Date()
+    hour.innerHTML = currentDay.getHours()
+    minute.innerHTML = currentDay.getMinutes()
+    second.innerHTML = currentDay.getSeconds()
+    
+},1000)
