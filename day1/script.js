d6dbe155534260562cf13c91d037d3f8c6941c07
5h140w <5h140w23@gmail.com>
2022-04-22 10:58:59 +0100

    day1

--- day1/script.js
+++ day1/script.js
@@ -2,10 +2,15 @@ const hour = document.querySelector("#hour")
 const minute = document.querySelector("#minute")
 const second = document.querySelector("#second")
 
-setInterval(()=>{
+let birthday = new Date(2022,5,24)
+
+setInterval(()=>{    
     let currentDay = new Date()
-    hour.innerHTML = currentDay.getHours()
-    minute.innerHTML = currentDay.getMinutes()
-    second.innerHTML = currentDay.getSeconds()
-    
+    let delta = Math.floor((birthday-currentDay) / 1000)
+    let deltaInSecond = delta %60
+    let deltaInMinute = Math.floor(delta/60)%(60)
+    let deltaInHours = Math.floor(delta/3600)
+    hour.innerHTML = deltaInHours
+    minute.innerHTML = deltaInMinute
+    second.innerHTML = deltaInSecond 
 },1000)
