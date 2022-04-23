c8dce9f9b1c26cceca857fa64b54c0af29320907
5h140w <5h140w23@gmail.com>
2022-04-23 17:30:43 +0100

    project1

--- Countdown/script.js
+++ Countdown/script.js
@@ -1,6 +1,7 @@
 const hour = document.querySelector("#hour")
 const minute = document.querySelector("#minute")
 const second = document.querySelector("#second")
+const day = document.querySelector("#day")
 
 let birthday = new Date(2022,5,24)
 
@@ -9,8 +10,10 @@ setInterval(()=>{
     let delta = Math.floor((birthday-currentDay) / 1000)
     let deltaInSecond = delta %60
     let deltaInMinute = Math.floor(delta/60)%(60)
-    let deltaInHours = Math.floor(delta/3600)
+    let deltaInHours = Math.floor(delta/3600) %24
+    let deltaInDays= Math.floor(delta /(3600*24))
+    day.innerHTML = deltaInDays
     hour.innerHTML = deltaInHours
     minute.innerHTML = deltaInMinute
-    second.innerHTML = deltaInSecond 
+    second.innerHTML = deltaInSecond
 },1000)
