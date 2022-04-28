1bb5ae3e72a75b70ff33e3c5b9f0c437af0dafc1
5h140w <5h140w23@gmail.com>
2022-04-28 18:03:45 +0100

    project11

--- scroll/script.js
+++ scroll/script.js
@@ -1,10 +1,17 @@
 const toogle = document.querySelector(".nav-toggle")
 const nav = document.querySelector(".menu")
-
+const banner = document.querySelector(".banner")
 
 toogle.addEventListener("click",()=>{
     nav.classList.toggle("show")
     const linksHeight = nav.getBoundingClientRect().height;
     console.log(linksHeight)
-
+    if(linksHeight==0){
+        banner.style.marginTop ="50px"
+    } else{
+        let xx = linksHeight + 50
+        console.log(typeof(linksHeight))
+        banner.style.marginTop = xx+"px"
+        console.log(banner)
+    }
 })
