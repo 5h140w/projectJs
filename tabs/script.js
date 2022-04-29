99fc396555553b31c2d24ecd7a0be673e9749ce3
5h140w <5h140w23@gmail.com>
2022-04-29 20:24:03 +0100

    project12

--- tabs/script.js
+++ tabs/script.js
@@ -1,1 +1,14 @@
-const btns = 
+const btns = document.querySelectorAll(".btn")
+const contents = document.querySelectorAll(".content")
+window.addEventListener("load",()=>{
+    btns.forEach((btn)=>{
+        if(btn.getAttribute("data-id")=="history"){
+            btn.classList.add("active")
+        }
+    })
+    contents.forEach((content)=>{
+        if(content.getAttribute("id")=="history"){
+            content.classList.add("show")
+        }
+    })
+})
