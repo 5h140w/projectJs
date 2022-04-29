10e23ab4e2417b5095997dc65dc0bc41985b306e
5h140w <5h140w23@gmail.com>
2022-04-29 17:01:37 +0100

    project11

--- scroll/script.js
+++ scroll/script.js
@@ -5,7 +5,25 @@ toggle.addEventListener("click",()=>{
     links.classList.toggle("show")
 })
 
+window.addEventListener("scroll",()=>{
+    const current_heigth = window.pageYOffset
+    if(current_heigth> 0){
+        nav.classList.add("fixed-nav")
+    }else{
+        nav.classList.remove("fixed-nav")
+    }
+    
+})
 
-const heigth = nav.getBoundingClientRect().height
 
-console.log(heigth)
+//const heigth = nav.getBoundingClientRect().height
+
+//console.log(heigth)
+
+const Alllinks = document.querySelectorAll(".link")
+
+Alllinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        console.log(link)
+    })
+})
