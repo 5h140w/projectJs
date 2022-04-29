b530ca45f855f11c2c3e5b39f57957784abbab35
5h140w <5h140w23@gmail.com>
2022-04-29 16:52:36 +0100

    project11

--- scroll/script.js
+++ scroll/script.js
@@ -1,6 +1,11 @@
 const toggle = document.querySelector(".nav-toggle")
 const links = document.querySelector(".linksContainer")
-
+const nav = document.querySelector("nav")
 toggle.addEventListener("click",()=>{
     links.classList.toggle("show")
 })
+
+
+const heigth = nav.getBoundingClientRect().height
+
+console.log(heigth)
