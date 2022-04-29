2b55e8b4ce93a5f6b350cc4d279da35566a9204a
5h140w <5h140w23@gmail.com>
2022-04-30 00:31:07 +0100

    project12

--- tabs/script.js
+++ tabs/script.js
@@ -1,5 +1,6 @@
 const btns = document.querySelectorAll(".btn")
 const contents = document.querySelectorAll(".content")
+const btnContainer = document.querySelector(".btns")
 window.addEventListener("load",()=>{
     btns.forEach((btn)=>{
         if(btn.getAttribute("data-id")=="history"){
@@ -11,4 +12,16 @@ window.addEventListener("load",()=>{
             content.classList.add("show")
         }
     })
+})
+
+btnContainer.addEventListener("click",(e)=>{
+    const id = e.target.dataset.id
+    btns.forEach((btn)=>{
+        btn.classList.remove("active")
+    })
+    e.target.classList.add("active")
+    contents.forEach((content)=>{
+        content.classList.remove("show")
+    })
+    document.getElementById(id).classList.add("show")
 })
