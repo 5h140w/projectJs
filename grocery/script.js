66aa73dc7f690925779111972309e5e566215145
5h140w <5h140w23@gmail.com>
2022-05-01 15:37:38 +0100

    project15

--- grocery/script.js
+++ grocery/script.js
@@ -1,5 +1,8 @@
-const GroceryList=["bacon","list1","list2"]
+let GroceryList=["bacon","list1","list2"]
 const items = document.querySelector(".items")
+const addButton = document.querySelector(".add")
+const name = document.querySelector("#name")
+const clearButton = document.querySelector("#clear")
 
 const showItems = () =>{
     const element = GroceryList.map((value,index)=>{
@@ -18,5 +21,17 @@ const showItems = () =>{
     items.innerHTML = element
 }
 window.addEventListener("load",()=>{
+    showItems()
+})
+
+
+addButton.addEventListener("click",(e)=>{
+    e.preventDefault()
+    GroceryList.push(name.value)
+    showItems()
+})
+
+clearButton.addEventListener("click",()=>{
+    GroceryList=[]
     showItems()
 })
