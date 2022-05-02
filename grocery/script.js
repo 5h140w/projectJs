54d2d4959c2c0862903fbec910476d4bd333a786
5h140w <5h140w23@gmail.com>
2022-05-02 14:46:44 +0100

    project15

--- grocery/script.js
+++ grocery/script.js
@@ -1,10 +1,9 @@
 let GroceryList=["bacon","list1","list2"]
 const items = document.querySelector(".items")
 const addButton = document.querySelector(".add")
-const name = document.querySelector("#name")
 const clearButton = document.querySelector("#clear")
 
-const showItems = (e) =>{
+const showItems = () =>{
     const element = GroceryList.map((value,index)=>{
         return ` <li class="item" data-id="${index}">
                     <p>${value}</p>
@@ -19,6 +18,23 @@ const showItems = (e) =>{
                 </li>`
     }) . join("")
     items.innerHTML = element
+
+    const Allitems = items.querySelectorAll(".item")
+    Allitems.forEach((item)=>{
+        const editButton = item.querySelector(".editButton")
+        editButton.addEventListener("click",(e)=>{
+            const id= e.currentTarget.parentElement.parentElement.dataset.id
+            const name = document.querySelector("#name")
+            GroceryList[id]= name.value
+            showItems() 
+        })
+        const deleteButton = item.querySelector(".deleteButton")
+        deleteButton.addEventListener("click",(e)=>{
+            const id= e.currentTarget.parentElement.parentElement.dataset.id
+            GroceryList.splice(id,1)
+            showItems() 
+        })
+    })
 }
 window.addEventListener("load",()=>{
     showItems()
@@ -27,6 +43,7 @@ window.addEventListener("load",()=>{
 
 addButton.addEventListener("click",(e)=>{
     e.preventDefault()
+    const name = document.querySelector("#name")
     GroceryList.push(name.value)
     showItems()
 })
