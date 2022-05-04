f77dc8e029dc35e63bc5401ef675ab19e0019f2d
5h140w <5h140w23@gmail.com>
2022-05-04 12:07:22 +0100

    project17

---
+++ cubeslider/script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+    effect: 'cube',
+    flipEffect: {
+        slideShadows: false,
+    },
+    // Optional parameters
+    direction: 'horizontal',
+    loop:true,
+    autoplay: {
+        delay: 5000,
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    });
