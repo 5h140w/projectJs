75de64836274e1deef0e6b50bc6142da06331477
5h140w <5h140w23@gmail.com>
2022-05-08 10:30:54 +0100

    project17

---
+++ calendar/script.js
@@ -0,0 +1,16 @@
+const day = new Date()
+const months=[
+    {"name":"January" , "nd":31}, 
+    {"name":"February", "nd":28} , 
+    {"name":"March" ,"nd":31},
+    { "name":"April" ,"nd":30}, 
+    {"name":"May" ,"nd":31} , 
+    {"name":"June" ,"nd":30}, 
+    {"name":"July" ,"nd":31} , 
+    {"name":"August" ,"nd":31} , 
+    {"name":"September" ,"nd":30} , 
+    {"name":"October" ,"nd":31} , 
+    {"name":"November" ,"nd":30} , 
+    {"name":"December" ,"nd":31}
+]
+console.log(day.getMonth())
