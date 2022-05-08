6cf2b217f05a1d7e87287aadc8b85bdfc369dadb
5h140w <5h140w23@gmail.com>
2022-05-08 10:50:00 +0100

    project17

--- calendar/script.js
+++ calendar/script.js
@@ -7,21 +7,36 @@ GetFebruaryDays = (year) =>{
     return isLeap(year) ? 29 :28
 }
 
+const generateCalendar = () =>{
+    let currentday = new Date()
+    let year = currentday.getFullYear()
+    let month = currentday.getMonth()
 
-const months=[
-    {"name":"January" , "nd":31}, 
-    {"name":"February", "nd":28} , 
-    {"name":"March" ,"nd":31},
-    { "name":"April" ,"nd":30}, 
-    {"name":"May" ,"nd":31} , 
-    {"name":"June" ,"nd":30}, 
-    {"name":"July" ,"nd":31} , 
-    {"name":"August" ,"nd":31} , 
-    {"name":"September" ,"nd":30} , 
-    {"name":"October" ,"nd":31} , 
-    {"name":"November" ,"nd":30} , 
-    {"name":"December" ,"nd":31}
-]
+    const months=[
+        {"name":"January" , "nd":31}, 
+        {"name":"February", "nd":GetFebruaryDays(year)} , 
+        {"name":"March" ,"nd":31},
+        { "name":"April" ,"nd":30}, 
+        {"name":"May" ,"nd":31} , 
+        {"name":"June" ,"nd":30}, 
+        {"name":"July" ,"nd":31} , 
+        {"name":"August" ,"nd":31} , 
+        {"name":"September" ,"nd":30} , 
+        {"name":"October" ,"nd":31} , 
+        {"name":"November" ,"nd":30} , 
+        {"name":"December" ,"nd":31}
+    ]
+    let first_day = new Date(year,month,1)
+    for (let i =0 ; i <= months[month] + first_day.getDay() - 1 ; i++){
+        let day = document.createElement("div")
+        if(i>= first_day.getDay()){
+            day.classList.add("calendar_day")
+            day.innerHTML = i - first_day.getDay() +1
+        }
+        
+    }
+}
+
 console.log(day.getMonth())
 
 console.log(months[4].name)
